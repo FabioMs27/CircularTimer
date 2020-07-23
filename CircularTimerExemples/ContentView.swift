944e99b1716988cf0ae8ee23ae9a84c7a8226575
Fábio Maciel de Sousa <fabio.ms27@hotmail.com>
2020-07-22 21:47:15 -0300

    Initial project setup

---
+++ CircularTimerExemples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircularTimerExemples
+//
+//  Created by Fábio Maciel de Sousa on 22/07/20.
+//  Copyright © 2020 Fábio Maciel de Sousa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CircularTimerExemplesTests/CircularTimerExemplesTests.swift
@@ -0,0 +1,34 @@
+//
+//  CircularTimerExemplesTests.swift
+//  CircularTimerExemplesTests
+//
+//  Created by Fábio Maciel de Sousa on 22/07/20.
+//  Copyright © 2020 Fábio Maciel de Sousa. All rights reserved.
+//
+
+import XCTest
+@testable import CircularTimerExemples
+
+class CircularTimerExemplesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
